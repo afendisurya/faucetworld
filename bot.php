938cb4c8d3cb3e2bbf889290fae4a81cb5e4d605
Afendi Surya <79998432+afendisurya@users.noreply.github.com>
2021-08-14 02:35:18 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,8 +1,31 @@
 <?php
 // HEAD
 
+/*
+
 // SERVER
-include('include.php');
+include('/home/asus/Desktop/0/header/header.php');
+include('/home/asus/Desktop/0/badan_atas/badan_atas.php');
+include('/home/asus/Desktop/0/curl/curl.php');
+include('/home/asus/Desktop/0/parsing/parsing.php');
+include('/home/asus/Desktop/0/countdown/countdown.php');
+include('/home/asus/Desktop/0/CLI/CLI.php');
+include('/home/asus/Desktop/0/warna/warna.php');
+include('/home/asus/Desktop/0/garis/garis.php');
+include('/home/asus/Desktop/0/notif/notif.php');
+
+*/
+
+// TERMUX
+include('0/header/header.php');
+include('0/badan_atas/badan_atas.php');
+include('0/curl/curl.php');
+include('0/parsing/parsing.php');
+include('0/countdown/countdown.php');
+include('0/CLI/CLI.php');
+include('0/warna/warna.php');
+include('0/garis/garis.php');
+include('0/notif/notif.php');
 
 // SCRIPT
 include('config.php');
@@ -29,33 +52,26 @@ $waktu = date('H:i:s');
 // CLEAR
 fungsi_reset();
 
-fungsi_badan_atas("FAUCETWORLD", "1.1.1");
+fungsi_badan_atas("FAUCETWORLD", "1.0.0");
 
 //echo "\n".$EC;
 
 echo "\n".$warna_isi_kuning_terang."Yuk Mulung...\n$warna_reset\n";
 
-$url_verify = "https://faucetworld.in/verify.php";
-$data_verify = "address=".$EC."&DOGE=on&refresh_time=60&booster=1&action=&g-recaptcha-response=";
-$cookie = $Coki;
-
-$var_untuk_fungsi_curl_post_verify = fungsi_curl_post($url_verify, $cookie, $data_verify);
-//echo $var_untuk_fungsi_curl_post_verify;
-
 while (true) {
 
-	$url = "https://faucetworld.in/payout-to-expresscrypto/?address=".$EC;
-	$cookie = $Coki;
+	$url = "https://faucetworld.in/payout-to-wallet/?address=".$EC;
+	$cookie = "address=".$EC.";currency=%5B%22DOGE%22%5D";
 
 	//echo "\n".$cookie;
 
 	// VAR
 	$var_untuk_fungsi_curl_get_web = fungsi_curl_get($url, $cookie);
 	$var_untuk_fungsi_parsing_html_ec = fungsi_parsing_html($var_untuk_fungsi_curl_get_web, '<span class="white-text">Welcome: ', "</span>", 1);
-	//$var_untuk_fungsi_parsing_html = fungsi_parsing_html($var_untuk_fungsi_curl_get_web, "<title>", "</title>", 1);
+	$var_untuk_fungsi_parsing_html = fungsi_parsing_html($var_untuk_fungsi_curl_get_web, "<title>", "</title>", 1);
 	$var_untuk_fungsi_parsing_html_coin = fungsi_parsing_html($var_untuk_fungsi_curl_get_web, '<div class="card center-align green darken-4 white-text z-depth-5 faa-horizontal animated">', '</div>', 1);
-	$var_untuk_fungsi_parsing_html_token = fungsi_parsing_html($var_untuk_fungsi_curl_get_web, '<title>
-        [', ']', 1);
+	$var_untuk_fungsi_parsing_html_token = fungsi_parsing_html($var_untuk_fungsi_curl_get_web, '<div class="card center-align orange darken-4 white-text z-depth-2">
+Your remaining AutoClaim Tokens: ', '<a', 1);
 
 	// ECHO
 	//echo "\n".$var_untuk_fungsi_curl_get_web;
@@ -64,11 +80,11 @@ while (true) {
 	//echo "\n".$var_untuk_fungsi_parsing_html_coin;
 	//echo "\n".$var_untuk_fungsi_parsing_html_token;
 
-	$var_teks = $waktu_sekarang." ID = ".$warna_isi_kuning_terang.$EC.$warna_reset." | PAYOUT = ".$warna_isi_hijau_terang.$var_untuk_fungsi_parsing_html_coin.$warna_reset." | TOKEN = ".$warna_isi_merah_terang.$var_untuk_fungsi_parsing_html_token.$warna_reset;
+	$var_teks = "[*] EC = ".$warna_isi_kuning_terang.$var_untuk_fungsi_parsing_html_ec.$warna_reset." | ".$warna_isi_hijau_terang.$var_untuk_fungsi_parsing_html_coin.$warna_reset." | Token = ".$warna_isi_merah_terang.$var_untuk_fungsi_parsing_html_token.$warna_reset;
 	echo $var_teks;
 
 echo "\n";
-fungsi_countdown(65, 0);
+fungsi_countdown(60, 0);
 //echo "\r$garis_dua";
 }
 ?>
